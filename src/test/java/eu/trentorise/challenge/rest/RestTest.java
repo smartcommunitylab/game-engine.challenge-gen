cee6ecbef9f3bb43b38c1168d2fda6dcb7405dc1
albertotn <amartinelli@fbk.eu>
2016-04-20 10:47:08 +0200

    moved stress test to https://github.com/das-fbk/game-engine.stress-test

--- src/test/java/eu/trentorise/challenge/rest/RestTest.java
+++ src/test/java/eu/trentorise/challenge/rest/RestTest.java
@@ -10,10 +10,7 @@ import static eu.trentorise.challenge.PropertiesUtil.USERNAME;
 import static eu.trentorise.challenge.PropertiesUtil.get;
 import static org.junit.Assert.assertTrue;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -31,152 +28,86 @@ import eu.trentorise.game.challenges.util.JourneyData;
 
 public class RestTest {
 
-    private GamificationEngineRestFacade facade;
-    private GamificationEngineRestFacade insertFacade;
+	private GamificationEngineRestFacade facade;
+	private GamificationEngineRestFacade insertFacade;
 
-    @Before
-    public void setup() {
-	facade = new GamificationEngineRestFacade(get(HOST) + get(CONTEXT),
-		get(USERNAME), get(PASSWORD));
-	insertFacade = new GamificationEngineRestFacade(get(HOST)
-		+ get(INSERT_CONTEXT), get(USERNAME), get(PASSWORD));
-    }
-
-    @Test
-    public void gameReadGameStateTest() {
-	List<Content> result = facade.readGameState(get(GAMEID));
-	assertTrue(!result.isEmpty());
-    }
-
-    @Test
-    public void gameInsertRuleTest() {
-	// define rule
-	InsertedRuleDto rule = new InsertedRuleDto();
-	rule.setContent("/* */");
-	rule.setName("sampleRule");
-	// insert rule
-	InsertedRuleDto result = insertFacade.insertGameRule(get(GAMEID), rule);
-	assertTrue(!result.getId().isEmpty());
-	// delete inserted rule
-	boolean res = insertFacade.deleteGameRule(get(GAMEID), result.getId());
-	assertTrue(res);
-    }
-
-    @Test
-    public void saveItineraryTest() {
-	GamificationEngineRestFacade facade = new GamificationEngineRestFacade(
-		get(HOST) + get(CONTEXT));
-	ExecutionDataDTO input = new ExecutionDataDTO();
-	input.setActionId(get(SAVE_ITINERARY));
-	input.setPlayerId("1");
-	input.setGameId(get(GAMEID));
-	Map<String, Object> data = new HashMap<String, Object>();
-	data.put("bikeDistance", Double.valueOf(1.0d));
-	input.setData(data);
-	boolean result = facade.saveItinerary(input);
-
-	assertTrue(result);
-    }
-
-    @Test
-    public void saveUsersItineraryLoadedFromFile() throws IOException {
-	// create input
-	String ref = "178-bus.json";
-
-	// read all lines from file
-	@SuppressWarnings("unchecked")
-	List<String> lines = IOUtils.readLines(Thread.currentThread()
-		.getContextClassLoader().getResourceAsStream(ref));
-
-	for (String line : lines) {
-	    JourneyData jd = ConverterUtil.extractJourneyData(line);
-	    ExecutionDataDTO input = new ExecutionDataDTO();
-	    input.setActionId(get(SAVE_ITINERARY));
-	    input.setPlayerId(jd.getUserId());
-	    input.setGameId(get(GAMEID));
-	    input.setData(jd.getData());
-	    boolean result = facade.saveItinerary(input);
-
-	    assertTrue(result);
-	}
-    }
-
-    @Test
-    public void updateChallengeCustomData() {
-	Map<String, Object> customData = new HashMap<String, Object>();
-	customData.put("target", "20");
-	boolean result = insertFacade.updateChallengeCustomData(get(GAMEID),
-		"178", customData);
-	assertTrue(result);
-	// reset custom data
-	// customData.put("target", "");
-	// result = insertFacade.updateChallengeCustomData(get(GAMEID), "178",
-	// customData);
-	// assertTrue(result);
-    }
-
-    // For stress test, first configure perf4j in game-engine.core, then see
-    // resulting log
-    @Test
-    public void stressTestRead() throws FileNotFoundException, IOException {
-	StringBuffer toWrite = new StringBuffer();
-
-	toWrite.append("N;TIME\n");
-	int n = 1000;
-	long start = 0;
-	long end = 0;
-	for (int i = 0; i < n; i++) {
-	    toWrite.append(i + ";");
-	    start = System.currentTimeMillis();
-	    List<Content> result = facade.readGameState(get(GAMEID));
-	    end = System.currentTimeMillis();
-	    toWrite.append(Math.abs(end - start) + "\n");
+	@Before
+	public void setup() {
+		facade = new GamificationEngineRestFacade(get(HOST) + get(CONTEXT),
+				get(USERNAME), get(PASSWORD));
+		insertFacade = new GamificationEngineRestFacade(get(HOST)
+				+ get(INSERT_CONTEXT), get(USERNAME), get(PASSWORD));
 	}
 
-	IOUtils.write(toWrite.toString(), new FileOutputStream(
-		"stressTestRead.csv"));
-    }
-
-    @Test
-    public void stressTestInsertAndRead() throws FileNotFoundException,
-	    IOException {
-	StringBuffer toWrite = new StringBuffer();
-
-	toWrite.append("N;TIME\n");
-	int n = 1000;
-	long start = 0;
-	long end = 0;
-
-	// insert 1000 rules
-
-	// define rule
-	List<String> inserted = new ArrayList<String>();
-	for (int i = 0; i < n; i++) {
-	    InsertedRuleDto rule = new InsertedRuleDto();
-	    rule.setContent("/* */");
-	    rule.setName("sampleRule" + i);
-	    // insert rule
-	    InsertedRuleDto result = insertFacade.insertGameRule(get(GAMEID),
-		    rule);
-	    inserted.add(result.getId());
+	@Test
+	public void gameReadGameStateTest() {
+		List<Content> result = facade.readGameState(get(GAMEID));
+		assertTrue(!result.isEmpty());
 	}
 
-	// write stress test report
-	for (int i = 0; i < n; i++) {
-	    toWrite.append(i + ";");
-	    start = System.currentTimeMillis();
-	    List<Content> result = facade.readGameState(get(GAMEID));
-	    end = System.currentTimeMillis();
-	    toWrite.append(Math.abs(end - start) + "\n");
+	@Test
+	public void gameInsertRuleTest() {
+		// define rule
+		InsertedRuleDto rule = new InsertedRuleDto();
+		rule.setContent("/* */");
+		rule.setName("sampleRule");
+		// insert rule
+		InsertedRuleDto result = insertFacade.insertGameRule(get(GAMEID), rule);
+		assertTrue(!result.getId().isEmpty());
+		// delete inserted rule
+		boolean res = insertFacade.deleteGameRule(get(GAMEID), result.getId());
+		assertTrue(res);
 	}
 
-	IOUtils.write(toWrite.toString(), new FileOutputStream(
-		"stressTestInsertAndRead.csv"));
+	@Test
+	public void saveItineraryTest() {
+		ExecutionDataDTO input = new ExecutionDataDTO();
+		input.setActionId(get(SAVE_ITINERARY));
+		input.setPlayerId("1");
+		input.setGameId(get(GAMEID));
+		Map<String, Object> data = new HashMap<String, Object>();
+		data.put("bikeDistance", Double.valueOf(1.0d));
+		input.setData(data);
+		boolean result = facade.saveItinerary(input);
 
-	// delete all inserted rules
-	for (String ruleId : inserted) {
-	    insertFacade.deleteGameRule(get(GAMEID), ruleId);
+		assertTrue(result);
 	}
-    }
+
+	@Test
+	public void saveUsersItineraryLoadedFromFile() throws IOException {
+		// create input
+		String ref = "178-bus.json";
+
+		// read all lines from file
+		@SuppressWarnings("unchecked")
+		List<String> lines = IOUtils.readLines(Thread.currentThread()
+				.getContextClassLoader().getResourceAsStream(ref));
+
+		for (String line : lines) {
+			JourneyData jd = ConverterUtil.extractJourneyData(line);
+			ExecutionDataDTO input = new ExecutionDataDTO();
+			input.setActionId(get(SAVE_ITINERARY));
+			input.setPlayerId(jd.getUserId());
+			input.setGameId(get(GAMEID));
+			input.setData(jd.getData());
+			boolean result = facade.saveItinerary(input);
+
+			assertTrue(result);
+		}
+	}
+
+	@Test
+	public void updateChallengeCustomData() {
+		Map<String, Object> customData = new HashMap<String, Object>();
+		customData.put("target", "20");
+		boolean result = insertFacade.updateChallengeCustomData(get(GAMEID),
+				"178", customData);
+		assertTrue(result);
+		// reset custom data
+		// customData.put("target", "");
+		// result = insertFacade.updateChallengeCustomData(get(GAMEID), "178",
+		// customData);
+		// assertTrue(result);
+	}
 
 }
