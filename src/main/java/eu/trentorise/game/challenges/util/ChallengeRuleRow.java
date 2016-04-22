ce3e926d99bcdc6906a128aba4c8e37e3d7566ef
albertotn <amartinelli@fbk.eu>
2016-04-22 09:48:16 +0200

    added toString

--- src/main/java/eu/trentorise/game/challenges/util/ChallengeRuleRow.java
+++ src/main/java/eu/trentorise/game/challenges/util/ChallengeRuleRow.java
@@ -2,96 +2,100 @@ package eu.trentorise.game.challenges.util;
 
 public class ChallengeRuleRow {
 
-    private String type;
-    private String goalType;
-    private Object target;
-    private String pointType;
-    private Integer bonus;
-    private String name;
-    private String baselineVar;
-    private String selectionCriteriaCustomData;
-    private String selectionCriteriaPoints;
-    private String selectionCriteriaBadges;
+	private String type;
+	private String goalType;
+	private Object target;
+	private String pointType;
+	private Integer bonus;
+	private String name;
+	private String baselineVar;
+	private String selectionCriteriaCustomData;
+	private String selectionCriteriaPoints;
+	private String selectionCriteriaBadges;
 
-    public String getType() {
-	return type;
-    }
+	public String getType() {
+		return type;
+	}
 
-    public void setType(String type) {
-	this.type = type;
-    }
+	public void setType(String type) {
+		this.type = type;
+	}
 
-    public String getGoalType() {
-	return goalType;
-    }
+	public String getGoalType() {
+		return goalType;
+	}
 
-    public void setGoalType(String goalType) {
-	this.goalType = goalType;
-    }
+	public void setGoalType(String goalType) {
+		this.goalType = goalType;
+	}
 
-    public Object getTarget() {
-	return target;
-    }
+	public Object getTarget() {
+		return target;
+	}
 
-    public void setTarget(Object target) {
-	this.target = target;
-    }
+	public void setTarget(Object target) {
+		this.target = target;
+	}
 
-    public String getPointType() {
-	return pointType;
-    }
+	public String getPointType() {
+		return pointType;
+	}
 
-    public void setPointType(String pointType) {
-	this.pointType = pointType;
-    }
+	public void setPointType(String pointType) {
+		this.pointType = pointType;
+	}
 
-    public void setSelectionCriteriaCustomData(
-	    String selectionCriteriaCustomData) {
-	this.selectionCriteriaCustomData = selectionCriteriaCustomData;
-    }
+	public void setSelectionCriteriaCustomData(
+			String selectionCriteriaCustomData) {
+		this.selectionCriteriaCustomData = selectionCriteriaCustomData;
+	}
 
-    public void setBonus(Integer bonus) {
-	this.bonus = bonus;
-    }
+	public void setBonus(Integer bonus) {
+		this.bonus = bonus;
+	}
 
-    public Integer getBonus() {
-	return bonus;
-    }
+	public Integer getBonus() {
+		return bonus;
+	}
 
-    public void setName(String name) {
-	this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getName() {
-	return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getBaselineVar() {
-	return baselineVar;
-    }
+	public String getBaselineVar() {
+		return baselineVar;
+	}
 
-    public void setBaselineVar(String baselineVar) {
-	this.baselineVar = baselineVar;
-    }
+	public void setBaselineVar(String baselineVar) {
+		this.baselineVar = baselineVar;
+	}
 
-    public void setSelectionCriteriaPoints(String selectionCriteriaPoints) {
-	this.selectionCriteriaPoints = selectionCriteriaPoints;
-    }
+	public void setSelectionCriteriaPoints(String selectionCriteriaPoints) {
+		this.selectionCriteriaPoints = selectionCriteriaPoints;
+	}
 
-    public String getSelectionCriteriaPoints() {
-	return selectionCriteriaPoints;
-    }
+	public String getSelectionCriteriaPoints() {
+		return selectionCriteriaPoints;
+	}
 
-    public String getSelectionCriteriaCustomData() {
-	return selectionCriteriaCustomData;
-    }
+	public String getSelectionCriteriaCustomData() {
+		return selectionCriteriaCustomData;
+	}
 
-    public void setSelectionCriteriaBadges(String selectionCriteriaBadges) {
-	this.selectionCriteriaBadges = selectionCriteriaBadges;
-    }
+	public void setSelectionCriteriaBadges(String selectionCriteriaBadges) {
+		this.selectionCriteriaBadges = selectionCriteriaBadges;
+	}
 
-    public String getSelectionCriteriaBadges() {
-	return selectionCriteriaBadges;
-    }
+	public String getSelectionCriteriaBadges() {
+		return selectionCriteriaBadges;
+	}
 
+	@Override
+	public String toString() {
+		return "[name=" + this.name + ",type=" + this.type + "]";
+	}
 }
