4e6d8c75dc5e6a2867a004c32c18747fd324423d
albertotn <amartinelli@fbk.eu>
2016-05-26 12:23:14 +0200

    added draft version for week 7 challenges

--- src/main/java/eu/trentorise/game/challenges/ChallengeFactory.java
+++ src/main/java/eu/trentorise/game/challenges/ChallengeFactory.java
@@ -7,36 +7,36 @@ import eu.trentorise.game.challenges.model.ChallengeType;
 
 public class ChallengeFactory implements ChallengeFactoryInterface {
 
-    @Override
-    public Challenge createChallenge(ChallengeType chType, String templateDir)
-	    throws UndefinedChallengeException {
-    	switch (chType) {
-			case PERCENT:
-			    return new PercentMobilityChallenge(templateDir);
-			case BSPERCENT:
-				return new BikeSharePercentMobilityChallenge(templateDir);
-			case TRIPNUMBER:
-			    return new TripNumberChallenge(templateDir);
-			case NEGATEDMODE:
-				return new NegatedModeChallenge(templateDir);
-			case BSTRIPNUMBER:
-				return new BikeShareTripNumberChallenge(templateDir);
-			case BADGECOLLECTION:
-			    return new BadgeCollectionCompletionChallenge(templateDir);
-			case RECOMMENDATION:
-			    return new RecommendationChallenge(templateDir);
-			case POINTSEARNED:
-				return new PointsEarnedChallenge(templateDir);
-			case ZEROIMPACT:
-				return new ZeroImpactChallenge(templateDir);
-			case NEXTBADGE:
-				return new NextBadgeChallenge(templateDir);
-			case LEADERBOARDPOSITION:
-				return new ClassificationImprovementChallenge(templateDir);
-			default:
-			    throw new UndefinedChallengeException("Unknown challenge type!"
-				    + chType.toString());
+	@Override
+	public Challenge createChallenge(ChallengeType chType, String templateDir)
+			throws UndefinedChallengeException {
+		switch (chType) {
+		case PERCENT:
+			return new PercentMobilityChallenge(templateDir);
+		case BSPERCENT:
+			return new BikeSharePercentMobilityChallenge(templateDir);
+		case TRIPNUMBER:
+			return new TripNumberChallenge(templateDir);
+		case NEGATEDMODE:
+			return new NegatedModeChallenge(templateDir);
+		case BSTRIPNUMBER:
+			return new BikeShareTripNumberChallenge(templateDir);
+		case BADGECOLLECTION:
+			return new BadgeCollectionCompletionChallenge(templateDir);
+		case RECOMMENDATION:
+			return new RecommendationChallenge(templateDir);
+		case POINTSEARNED:
+			return new PointsEarnedChallenge(templateDir);
+		case ZEROIMPACT:
+			return new ZeroImpactChallenge(templateDir);
+		case NEXTBADGE:
+			return new NextBadgeChallenge(templateDir);
+			// case LEADERBOARDPOSITION:
+			// return new ClassificationImprovementChallenge(templateDir);
+		default:
+			throw new UndefinedChallengeException("Unknown challenge type!"
+					+ chType.toString());
 		}
-    }
+	}
 
 }
