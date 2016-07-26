8cabf3dc1524adaf97752483080c6763c7644ed6
albertotn <amartinelli@fbk.eu>
2016-07-26 13:23:05 +0200

    added javadoc

--- src/main/java/eu/trentorise/game/challenges/util/ChallengeRules.java
+++ src/main/java/eu/trentorise/game/challenges/util/ChallengeRules.java
@@ -3,16 +3,19 @@ package eu.trentorise.game.challenges.util;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A simple list of {@link ChallengeRuleRow}
+ **/
 public class ChallengeRules {
 
-    private List<ChallengeRuleRow> challenges = new ArrayList<ChallengeRuleRow>();
+	private List<ChallengeRuleRow> challenges = new ArrayList<ChallengeRuleRow>();
 
-    public List<ChallengeRuleRow> getChallenges() {
-	return challenges;
-    }
+	public List<ChallengeRuleRow> getChallenges() {
+		return challenges;
+	}
 
-    public void setChallenges(List<ChallengeRuleRow> challenges) {
-	this.challenges = challenges;
-    }
+	public void setChallenges(List<ChallengeRuleRow> challenges) {
+		this.challenges = challenges;
+	}
 
 }
