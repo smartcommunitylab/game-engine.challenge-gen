f5f65b2c00e7d964362cf0dafccdf8508326e6c9
mirkoperillo <mirko.perillo@gmail.com>
2017-09-26 14:49:01 +0200

    fix for checking challenge

--- src/main/java/eu/trentorise/game/challenges/ChallengeInstanceFactory.java
+++ src/main/java/eu/trentorise/game/challenges/ChallengeInstanceFactory.java
@@ -13,183 +13,185 @@ import eu.trentorise.game.challenges.rest.Content;
 
 public class ChallengeInstanceFactory {
 
-	public ChallengeDataDTO createChallenge(String chType,
-			Map<String, Object> params, Content user)
-			throws UndefinedChallengeException {
-		switch (chType) {
-		case Constants.ABSOLUTEINCREMENT:
-			return buildAbsoluteIncrement(params);
-		case Constants.PERCENTAGEINCREMENT:
-			return buildPercentageIncrement(params);
-		case Constants.NEXTBADGE:
-			return buildNextBadge(params, user);
-		case Constants.COMPLETEBADGECOLLECTION:
-			return buildCompleteBadgeCollection(params, user);
-		case Constants.SURVEY:
-			return buildSurvey(params, user);
-		case Constants.POICHECKIN:
-			return buildPoiCheckIn(params, user);
-		case Constants.LEADERBOARDPOSITION:
-			return buildLeaderBoardPosition(params, user);
+    public ChallengeDataDTO createChallenge(String chType, Map<String, Object> params, Content user)
+            throws UndefinedChallengeException {
+        switch (chType) {
+            case Constants.ABSOLUTEINCREMENT:
+                return buildAbsoluteIncrement(params);
+            case Constants.PERCENTAGEINCREMENT:
+                return buildPercentageIncrement(params);
+            case Constants.NEXTBADGE:
+                return buildNextBadge(params, user);
+            case Constants.COMPLETEBADGECOLLECTION:
+                return buildCompleteBadgeCollection(params, user);
+            case Constants.SURVEY:
+                return buildSurvey(params, user);
+            case Constants.POICHECKIN:
+                return buildPoiCheckIn(params, user);
+            case Constants.CHECKIN:
+                return buildCheckIn(params, user);
+            case Constants.LEADERBOARDPOSITION:
+                return buildLeaderBoardPosition(params, user);
 
-		default:
-			throw new UndefinedChallengeException("Unknown challenge type! "
-					+ chType.toString());
-		}
-	}
+            default:
+                throw new UndefinedChallengeException(
+                        "Challenge Generator doesn't recognize this challenge type! "
+                                + chType.toString());
+        }
+    }
 
-	private ChallengeDataDTO buildSurvey(Map<String, Object> params,
-			Content user) {
-		ChallengeDataDTO cdd = new ChallengeDataDTO();
-		cdd.setModelName(Constants.SURVEY);
-		cdd.setInstanceName(params.get(Constants.NAME) + "_"
-				+ UUID.randomUUID());
-		cdd.setStart((Date) params.get(Constants.START_DATE));
-		cdd.setEnd((Date) params.get(Constants.END_DATE));
-		Map<String, Object> data = new HashMap<String, Object>();
-		data.put(Constants.BONUS_POINT_TYPE,
-				params.get(Constants.BONUS_POINT_TYPE));
-		data.put(Constants.BONUS_SCORE,
-				Double.valueOf(params.get(Constants.BONUS_SCORE).toString()));
-		cdd.setData(data);
-		return cdd;
-	}
+    private ChallengeDataDTO buildSurvey(Map<String, Object> params, Content user) {
+        ChallengeDataDTO cdd = new ChallengeDataDTO();
+        cdd.setModelName(Constants.SURVEY);
+        cdd.setInstanceName(params.get(Constants.NAME) + "_" + UUID.randomUUID());
+        cdd.setStart((Date) params.get(Constants.START_DATE));
+        cdd.setEnd((Date) params.get(Constants.END_DATE));
+        Map<String, Object> data = new HashMap<String, Object>();
+        data.put(Constants.BONUS_POINT_TYPE, params.get(Constants.BONUS_POINT_TYPE));
+        data.put(Constants.BONUS_SCORE,
+                Double.valueOf(params.get(Constants.BONUS_SCORE).toString()));
+        cdd.setData(data);
+        return cdd;
+    }
 
-	private ChallengeDataDTO buildCompleteBadgeCollection(
-			Map<String, Object> params, Content user) {
-		ChallengeDataDTO cdd = new ChallengeDataDTO();
-		cdd.setModelName(Constants.COMPLETEBADGECOLLECTION);
-		cdd.setInstanceName(params.get(Constants.NAME) + "_"
-				+ UUID.randomUUID());
-		cdd.setStart((Date) params.get(Constants.START_DATE));
-		cdd.setEnd((Date) params.get(Constants.END_DATE));
-		Map<String, Object> data = new HashMap<String, Object>();
-		data.put(Constants.TARGET, params.get(Constants.TARGET));
-		data.put(Constants.BONUS_POINT_TYPE,
-				params.get(Constants.BONUS_POINT_TYPE));
-		data.put(Constants.BONUS_SCORE,
-				Double.valueOf(params.get(Constants.BONUS_SCORE).toString()));
-		data.put(Constants.BADGECOLLECTIONNAME, params.get(Constants.GOAL_TYPE));
-		cdd.setData(data);
-		return cdd;
-	}
+    private ChallengeDataDTO buildCompleteBadgeCollection(Map<String, Object> params,
+            Content user) {
+        ChallengeDataDTO cdd = new ChallengeDataDTO();
+        cdd.setModelName(Constants.COMPLETEBADGECOLLECTION);
+        cdd.setInstanceName(params.get(Constants.NAME) + "_" + UUID.randomUUID());
+        cdd.setStart((Date) params.get(Constants.START_DATE));
+        cdd.setEnd((Date) params.get(Constants.END_DATE));
+        Map<String, Object> data = new HashMap<String, Object>();
+        data.put(Constants.TARGET, params.get(Constants.TARGET));
+        data.put(Constants.BONUS_POINT_TYPE, params.get(Constants.BONUS_POINT_TYPE));
+        data.put(Constants.BONUS_SCORE,
+                Double.valueOf(params.get(Constants.BONUS_SCORE).toString()));
+        data.put(Constants.BADGECOLLECTIONNAME, params.get(Constants.GOAL_TYPE));
+        cdd.setData(data);
+        return cdd;
+    }
 
-	private ChallengeDataDTO buildNextBadge(Map<String, Object> params,
-			Content user) {
-		ChallengeDataDTO cdd = new ChallengeDataDTO();
-		cdd.setModelName(Constants.NEXTBADGE);
-		cdd.setInstanceName(params.get(Constants.NAME) + "_"
-				+ UUID.randomUUID());
-		cdd.setStart((Date) params.get(Constants.START_DATE));
-		cdd.setEnd((Date) params.get(Constants.END_DATE));
-		Map<String, Object> data = new HashMap<String, Object>();
-		data.put(Constants.TARGET, params.get(Constants.TARGET));
-		data.put(Constants.BONUS_POINT_TYPE,
-				params.get(Constants.BONUS_POINT_TYPE));
-		data.put(Constants.BONUS_SCORE,
-				Double.valueOf(params.get(Constants.BONUS_SCORE).toString()));
-		data.put(Constants.BADGECOLLECTIONNAME, params.get(Constants.GOAL_TYPE));
-		data.put(
-				Constants.INITIAL_BADGE_NUMBER,
-				getCurrentBadgeCollectionSize(user,
-						(String) params.get(Constants.GOAL_TYPE)));
+    private ChallengeDataDTO buildNextBadge(Map<String, Object> params, Content user) {
+        ChallengeDataDTO cdd = new ChallengeDataDTO();
+        cdd.setModelName(Constants.NEXTBADGE);
+        cdd.setInstanceName(params.get(Constants.NAME) + "_" + UUID.randomUUID());
+        cdd.setStart((Date) params.get(Constants.START_DATE));
+        cdd.setEnd((Date) params.get(Constants.END_DATE));
+        Map<String, Object> data = new HashMap<String, Object>();
+        data.put(Constants.TARGET, params.get(Constants.TARGET));
+        data.put(Constants.BONUS_POINT_TYPE, params.get(Constants.BONUS_POINT_TYPE));
+        data.put(Constants.BONUS_SCORE,
+                Double.valueOf(params.get(Constants.BONUS_SCORE).toString()));
+        data.put(Constants.BADGECOLLECTIONNAME, params.get(Constants.GOAL_TYPE));
+        data.put(Constants.INITIAL_BADGE_NUMBER,
+                getCurrentBadgeCollectionSize(user, (String) params.get(Constants.GOAL_TYPE)));
 
-		cdd.setData(data);
-		return cdd;
-	}
+        cdd.setData(data);
+        return cdd;
+    }
 
-	private ChallengeDataDTO buildPercentageIncrement(Map<String, Object> params) {
-		ChallengeDataDTO cdd = new ChallengeDataDTO();
-		cdd.setModelName(Constants.PERCENTAGEINCREMENT);
-		cdd.setInstanceName(params.get(Constants.NAME) + "_"
-				+ UUID.randomUUID());
-		cdd.setStart((Date) params.get(Constants.START_DATE));
-		cdd.setEnd((Date) params.get(Constants.END_DATE));
-		Map<String, Object> data = new HashMap<String, Object>();
-		data.put(Constants.COUNTER_NAME, params.get(Constants.GOAL_TYPE));
-		data.put(Constants.PERIOD_NAME, params.get(Constants.PERIOD_NAME));
-		data.put(Constants.TARGET, params.get(Constants.TARGET));
-		data.put(Constants.BASELINE, params.get(Constants.BASELINE));
-		data.put(Constants.BONUS_POINT_TYPE,
-				params.get(Constants.BONUS_POINT_TYPE));
-		data.put(Constants.BONUS_SCORE,
-				Double.valueOf(params.get(Constants.BONUS_SCORE).toString()));
-		cdd.setData(data);
-		return cdd;
-	}
+    private ChallengeDataDTO buildPercentageIncrement(Map<String, Object> params) {
+        ChallengeDataDTO cdd = new ChallengeDataDTO();
+        cdd.setModelName(Constants.PERCENTAGEINCREMENT);
+        cdd.setInstanceName(params.get(Constants.NAME) + "_" + UUID.randomUUID());
+        cdd.setStart((Date) params.get(Constants.START_DATE));
+        cdd.setEnd((Date) params.get(Constants.END_DATE));
+        Map<String, Object> data = new HashMap<String, Object>();
+        data.put(Constants.COUNTER_NAME, params.get(Constants.GOAL_TYPE));
+        data.put(Constants.PERIOD_NAME, params.get(Constants.PERIOD_NAME));
+        data.put(Constants.TARGET, params.get(Constants.TARGET));
+        data.put(Constants.BASELINE, params.get(Constants.BASELINE));
+        data.put(Constants.BONUS_POINT_TYPE, params.get(Constants.BONUS_POINT_TYPE));
+        data.put(Constants.BONUS_SCORE,
+                Double.valueOf(params.get(Constants.BONUS_SCORE).toString()));
+        cdd.setData(data);
+        return cdd;
+    }
 
-	private ChallengeDataDTO buildAbsoluteIncrement(Map<String, Object> params) {
-		ChallengeDataDTO cdd = new ChallengeDataDTO();
-		cdd.setModelName(Constants.ABSOLUTEINCREMENT);
-		cdd.setInstanceName(params.get(Constants.NAME) + "_"
-				+ UUID.randomUUID());
-		cdd.setStart((Date) params.get(Constants.START_DATE));
-		cdd.setEnd((Date) params.get(Constants.END_DATE));
-		Map<String, Object> data = new HashMap<String, Object>();
-		data.put(Constants.COUNTER_NAME, params.get(Constants.GOAL_TYPE));
-		data.put(Constants.PERIOD_NAME, params.get(Constants.PERIOD_NAME));
-		data.put(Constants.TARGET, params.get(Constants.TARGET));
-		data.put(Constants.BONUS_POINT_TYPE,
-				params.get(Constants.BONUS_POINT_TYPE));
-		data.put(Constants.BONUS_SCORE,
-				Double.valueOf(params.get(Constants.BONUS_SCORE).toString()));
-		cdd.setData(data);
-		return cdd;
-	}
+    private ChallengeDataDTO buildAbsoluteIncrement(Map<String, Object> params) {
+        ChallengeDataDTO cdd = new ChallengeDataDTO();
+        cdd.setModelName(Constants.ABSOLUTEINCREMENT);
+        cdd.setInstanceName(params.get(Constants.NAME) + "_" + UUID.randomUUID());
+        cdd.setStart((Date) params.get(Constants.START_DATE));
+        cdd.setEnd((Date) params.get(Constants.END_DATE));
+        Map<String, Object> data = new HashMap<String, Object>();
+        data.put(Constants.COUNTER_NAME, params.get(Constants.GOAL_TYPE));
+        data.put(Constants.PERIOD_NAME, params.get(Constants.PERIOD_NAME));
+        data.put(Constants.TARGET, params.get(Constants.TARGET));
+        data.put(Constants.BONUS_POINT_TYPE, params.get(Constants.BONUS_POINT_TYPE));
+        data.put(Constants.BONUS_SCORE,
+                Double.valueOf(params.get(Constants.BONUS_SCORE).toString()));
+        cdd.setData(data);
+        return cdd;
+    }
 
-	private ChallengeDataDTO buildPoiCheckIn(Map<String, Object> params,
-			Content user) {
+    private ChallengeDataDTO buildPoiCheckIn(Map<String, Object> params, Content user) {
 
-		ChallengeDataDTO cdd = new ChallengeDataDTO();
-		cdd.setModelName(Constants.POICHECKIN);
-		cdd.setInstanceName(params.get(Constants.NAME) + "_"
-				+ UUID.randomUUID());
-		cdd.setStart((Date) params.get(Constants.START_DATE));
-		cdd.setEnd((Date) params.get(Constants.END_DATE));
-		Map<String, Object> data = new HashMap<String, Object>();
-		data.put(Constants.POI_NAME, "Trento Fiera");
-		data.put(Constants.EVENT_NAME, "Fai la cosa giusta");
-		data.put(Constants.POI_STATE, Boolean.FALSE);
-		data.put(Constants.EVENT_STATE, Boolean.FALSE);
-		data.put(Constants.BONUS_POINT_TYPE,
-				params.get(Constants.BONUS_POINT_TYPE));
-		data.put(Constants.BONUS_SCORE,
-				Double.valueOf(params.get(Constants.BONUS_SCORE).toString()));
+        ChallengeDataDTO cdd = new ChallengeDataDTO();
+        cdd.setModelName(Constants.POICHECKIN);
+        cdd.setInstanceName(params.get(Constants.NAME) + "_" + UUID.randomUUID());
+        cdd.setStart((Date) params.get(Constants.START_DATE));
+        cdd.setEnd((Date) params.get(Constants.END_DATE));
+        Map<String, Object> data = new HashMap<String, Object>();
+        data.put(Constants.POI_NAME, "Trento Fiera");
+        data.put(Constants.EVENT_NAME, "Fai la cosa giusta");
+        data.put(Constants.POI_STATE, Boolean.FALSE);
+        data.put(Constants.EVENT_STATE, Boolean.FALSE);
+        data.put(Constants.BONUS_POINT_TYPE, params.get(Constants.BONUS_POINT_TYPE));
+        data.put(Constants.BONUS_SCORE,
+                Double.valueOf(params.get(Constants.BONUS_SCORE).toString()));
 
-		cdd.setData(data);
-		return cdd;
-	}
+        cdd.setData(data);
+        return cdd;
+    }
 
-	private Integer getCurrentBadgeCollectionSize(Content user, String name) {
-		for (BadgeCollectionConcept bc : user.getState()
-				.getBadgeCollectionConcept()) {
-			if (bc.getName().equals(name)) {
-				return bc.getBadgeEarned().size();
-			}
-		}
-		return 0;
-	}
+    private ChallengeDataDTO buildCheckIn(Map<String, Object> params, Content user) {
 
-	private ChallengeDataDTO buildLeaderBoardPosition(
-			Map<String, Object> params, Content user) {
-		ChallengeDataDTO cdd = new ChallengeDataDTO();
-		cdd.setModelName(Constants.LEADERBOARDPOSITION);
-		cdd.setInstanceName(params.get(Constants.NAME) + "_"
-				+ UUID.randomUUID());
-		cdd.setStart((Date) params.get(Constants.START_DATE));
-		cdd.setEnd((Date) params.get(Constants.END_DATE));
-		Map<String, Object> data = new HashMap<String, Object>();
-		String[] values = ((String) params.get(Constants.TARGET))
-				.split(Constants.MIN_MAX_SEPARATOR);
-		data.put("posMin", Double.valueOf(values[0]));
-		data.put("posMax", Double.valueOf(values[1]));
-		data.put("bonusPointType", params.get(Constants.BONUS_POINT_TYPE));
-		data.put("bonusScore",
-				Double.valueOf(params.get(Constants.BONUS_SCORE).toString()));
-		data.put("weekClassificationName", params.get(Constants.GOAL_TYPE));
+        ChallengeDataDTO cdd = new ChallengeDataDTO();
+        cdd.setModelName(Constants.CHECKIN);
+        cdd.setInstanceName(params.get(Constants.NAME) + "_" + UUID.randomUUID());
+        cdd.setStart((Date) params.get(Constants.START_DATE));
+        cdd.setEnd((Date) params.get(Constants.END_DATE));
+        Map<String, Object> data = new HashMap<String, Object>();
+        data.put("checkinType", params.get(Constants.GOAL_TYPE)); // reused an excel existing field
+                                                                  // for this
+                                                                  // type of challenge
+        data.put(Constants.BONUS_POINT_TYPE, params.get(Constants.BONUS_POINT_TYPE));
+        data.put(Constants.BONUS_SCORE,
+                Double.valueOf(params.get(Constants.BONUS_SCORE).toString()));
 
-		cdd.setData(data);
-		return cdd;
-	}
+        cdd.setData(data);
+        return cdd;
+    }
+
+
+
+    private Integer getCurrentBadgeCollectionSize(Content user, String name) {
+        for (BadgeCollectionConcept bc : user.getState().getBadgeCollectionConcept()) {
+            if (bc.getName().equals(name)) {
+                return bc.getBadgeEarned().size();
+            }
+        }
+        return 0;
+    }
+
+    private ChallengeDataDTO buildLeaderBoardPosition(Map<String, Object> params, Content user) {
+        ChallengeDataDTO cdd = new ChallengeDataDTO();
+        cdd.setModelName(Constants.LEADERBOARDPOSITION);
+        cdd.setInstanceName(params.get(Constants.NAME) + "_" + UUID.randomUUID());
+        cdd.setStart((Date) params.get(Constants.START_DATE));
+        cdd.setEnd((Date) params.get(Constants.END_DATE));
+        Map<String, Object> data = new HashMap<String, Object>();
+        String[] values =
+                ((String) params.get(Constants.TARGET)).split(Constants.MIN_MAX_SEPARATOR);
+        data.put("posMin", Double.valueOf(values[0]));
+        data.put("posMax", Double.valueOf(values[1]));
+        data.put("bonusPointType", params.get(Constants.BONUS_POINT_TYPE));
+        data.put("bonusScore", Double.valueOf(params.get(Constants.BONUS_SCORE).toString()));
+        data.put("weekClassificationName", params.get(Constants.GOAL_TYPE));
+
+        cdd.setData(data);
+        return cdd;
+    }
 
 }
--- src/main/java/eu/trentorise/game/challenges/api/Constants.java
+++ src/main/java/eu/trentorise/game/challenges/api/Constants.java
@@ -2,66 +2,64 @@ package eu.trentorise.game.challenges.api;
 
 public final class Constants {
 
-	// private constructor to avoid instantiations
-	private Constants() {
-	}
+    // private constructor to avoid instantiations
+    private Constants() {}
 
-	// General constants
-	public static final String LINE_SEPARATOR = System
-			.getProperty("line.separator");
+    // General constants
+    public static final String LINE_SEPARATOR = System.getProperty("line.separator");
 
-	// Challenge constants
-	public static final String fileExt = ".drl";
-	public static final String CH = "ch_";
-	public static final String TYPE = "_type";
-	public static final String START_CHTS = "_startChTs";
-	public static final String END_CHTS = "_endChTs";
-	public static final String SUCCESS = "_success";
-	public static final String CH_ID = "ch_ID";
-	public static final Object MODE = "mode";
-	public static final String CH_MODE = "ch_mode";
-	public static final Object POINT_TYPE = "point_type";
-	public static final String CH_POINT_TYPE = "ch_point_type";
-	public static final String RULE_PREFIX = "db://";
+    // Challenge constants
+    public static final String fileExt = ".drl";
+    public static final String CH = "ch_";
+    public static final String TYPE = "_type";
+    public static final String START_CHTS = "_startChTs";
+    public static final String END_CHTS = "_endChTs";
+    public static final String SUCCESS = "_success";
+    public static final String CH_ID = "ch_ID";
+    public static final Object MODE = "mode";
+    public static final String CH_MODE = "ch_mode";
+    public static final Object POINT_TYPE = "point_type";
+    public static final String CH_POINT_TYPE = "ch_point_type";
+    public static final String RULE_PREFIX = "db://";
 
-	// challenges type constants (v2.1.0)
-	public static final String ABSOLUTEINCREMENT = "absoluteIncrement";
-	public static final String PERCENTAGEINCREMENT = "percentageIncrement";
-	public static final String NEXTBADGE = "nextBadge";
-	public static final String COMPLETEBADGECOLLECTION = "completeBadgeCollection";
-	public static final String POICHECKIN = "poiCheckin";
-	public static final String LEADERBOARDPOSITION = "leaderboardPosition";
+    // challenges type constants (v2.1.0)
+    public static final String ABSOLUTEINCREMENT = "absoluteIncrement";
+    public static final String PERCENTAGEINCREMENT = "percentageIncrement";
+    public static final String NEXTBADGE = "nextBadge";
+    public static final String COMPLETEBADGECOLLECTION = "completeBadgeCollection";
+    public static final String POICHECKIN = "poiCheckin";
+    public static final String LEADERBOARDPOSITION = "leaderboardPosition";
 
-	// challenges various constants
-	public static final String NAME = "name";
-	public static final String BONUS_SCORE = "bonusScore";
-	public static final String BONUS_POINT_TYPE = "bonusPointType";
-	public static final String TARGET = "target";
-	public static final String COUNTER = "counter";
-	public static final String END_DATE = "endDate";
-	public static final String START_DATE = "startDate";
-	public static final String ZEROIMPACT = "zeroImpact";
-	public static final String COUNTER_NAME = "counterName";
-	public static final String PERIOD_NAME = "periodName";
-	public static final String BASELINE = "baseline";
-	public static final String BADGECOLLECTIONNAME = "badgeCollectionName";
-	public static final String GOAL_TYPE = "goalType";
-	public static final String INITIAL_BADGE_NUMBER = "initialBadgeNum";
-	public static final String SURVEY = "survey";
-	public static final String COMPLETED = "completed";
-	public static final String DATE_COMPLETED = "dateCompleted";
-	public static final String POI_NAME = "poiName";
-	public static final String EVENT_NAME = "eventName";
-	public static final String POI_STATE = "poiState";
-	public static final String EVENT_STATE = "eventState";
-	public static final String MIN_MAX_SEPARATOR = "<";
+    public static final String CHECKIN = "checkin";
 
-	// counters for rules
-	public static final String[] COUNTERS = { "gp_current",
-			"zero_impact_trips_past", "walk_km_past", "walk_trips_past",
-			"bike_km_past", "bike_trips_past", "bikesharing_km_past",
-			"bikesharing_trips_past", "bus_km_past", "bus_trips_past",
-			"train_km_past", "train_trips_past", "car_km_past",
-			"car_trips_past" };
+    // challenges various constants
+    public static final String NAME = "name";
+    public static final String BONUS_SCORE = "bonusScore";
+    public static final String BONUS_POINT_TYPE = "bonusPointType";
+    public static final String TARGET = "target";
+    public static final String COUNTER = "counter";
+    public static final String END_DATE = "endDate";
+    public static final String START_DATE = "startDate";
+    public static final String ZEROIMPACT = "zeroImpact";
+    public static final String COUNTER_NAME = "counterName";
+    public static final String PERIOD_NAME = "periodName";
+    public static final String BASELINE = "baseline";
+    public static final String BADGECOLLECTIONNAME = "badgeCollectionName";
+    public static final String GOAL_TYPE = "goalType";
+    public static final String INITIAL_BADGE_NUMBER = "initialBadgeNum";
+    public static final String SURVEY = "survey";
+    public static final String COMPLETED = "completed";
+    public static final String DATE_COMPLETED = "dateCompleted";
+    public static final String POI_NAME = "poiName";
+    public static final String EVENT_NAME = "eventName";
+    public static final String POI_STATE = "poiState";
+    public static final String EVENT_STATE = "eventState";
+    public static final String MIN_MAX_SEPARATOR = "<";
+
+    // counters for rules
+    public static final String[] COUNTERS = {"gp_current", "zero_impact_trips_past", "walk_km_past",
+            "walk_trips_past", "bike_km_past", "bike_trips_past", "bikesharing_km_past",
+            "bikesharing_trips_past", "bus_km_past", "bus_trips_past", "train_km_past",
+            "train_trips_past", "car_km_past", "car_trips_past"};
 
 }
