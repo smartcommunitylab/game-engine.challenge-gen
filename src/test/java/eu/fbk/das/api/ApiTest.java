456973d756a0c874474fe318382879b1d78ef15b
mirkoperillo <mirko.perillo@gmail.com>
2020-07-16 10:10:25 +0200

    fix the test fails to compile

--- src/test/java/eu/fbk/das/api/ApiTest.java
+++ src/test/java/eu/fbk/das/api/ApiTest.java
@@ -1,20 +1,20 @@
 package eu.fbk.das.api;
 
+import static eu.fbk.das.rs.utils.Utils.p;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+
 import eu.fbk.das.GamificationEngineRestFacade;
 import eu.fbk.das.api.exec.RecommenderSystemGroup;
 import eu.fbk.das.api.exec.RecommenderSystemTantum;
 import eu.fbk.das.api.exec.RecommenderSystemWeekly;
-import eu.fbk.das.model.ChallengeExpandedDTO;
 import it.smartcommunitylab.model.PlayerStateDTO;
 import it.smartcommunitylab.model.ext.GameConcept;
-import org.junit.Before;
-import org.junit.Test;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Set;
-
-import static eu.fbk.das.rs.utils.Utils.p;
 
 public class ApiTest {
 
@@ -76,8 +76,8 @@ public class ApiTest {
     @Test
     public void testSingle() {
         RecommenderSystemTantum rsw = new RecommenderSystemTantum();
-
-        rsw.exec(conf, "1069", "challengeType");
+        Map<String, Object> params = new HashMap<>();
+        rsw.exec(conf, "model", params, "1069");
     }
 
 }
